4307e3b05876042ab076ae1ddb211e2c05aa1346
joasand-rr <jsn@riksrevisjonen.no>
2025-09-08 08:42:40 +0200

    new theme elements

--- R/specific-themes.R
+++ R/specific-themes.R
@@ -43,9 +43,9 @@ geom_bar_RR <- function(..., width = 0.6){
 #'
 geom_bar_flipped_RR <- function(){
   list(geom_bar_RR(),
-       ggplot2::theme(panel.grid.major.x = ggplot2::element_line(color = scales::alpha(RR_dark_blue, 0.5),
-                                                        linewidth = ggplot2::rel(1.75),
-                                                        linetype = "dotted", size = 0.6),
+       ggplot2::theme(panel.grid.major.x = ggplot2::element_line(RR_gridlines_color,
+                                                                 linewidth = ggplot2::rel(1.75),
+                                                                 linetype = "dotted", size = 0.6),
                       panel.grid.major.y = ggplot2::element_blank(),
                       legend.position = "bottom"),
        ggplot2::coord_flip())
@@ -72,12 +72,12 @@ geom_bar_flipped_RR <- function(){
 #'
 geom_point_RR <- function(..., size = 2.5){
   list(ggplot2::geom_point(size = size),
-       ggplot2::theme(panel.grid.major.x = ggplot2::element_line(color = scales::alpha(RR_dark_blue, 0.5),
-                                                        linewidth = ggplot2::rel(1.75),
-                                                        linetype = "dotted", size = 0.6),
-                      panel.grid.major.y = ggplot2::element_line(color = scales::alpha(RR_dark_blue, 0.5),
-                                                        linewidth = ggplot2::rel(1.75),
-                                                        linetype = "dotted", size = 0.6)),
+       ggplot2::theme(panel.grid.major.x = ggplot2::element_line(color = RR_gridlines_color,
+                                                                 linewidth = RR_gridlines_size,
+                                                                 linetype = "solid", size = 0.6),
+                      panel.grid.major.y = ggplot2::element_line(color = RR_gridlines_color,
+                                                                 linewidth = RR_gridlines_size,
+                                                                 linetype = "solid", size = 0.6)),
        ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = c(0.1,0.1))))
 }
 
@@ -99,12 +99,12 @@ geom_point_RR <- function(..., size = 2.5){
 #'
 geom_jitter_RR <- function(..., size = 2.5, width = 0.4, height = 0.4){
   list(ggplot2::geom_jitter(size = size, width = width, height = height),
-       ggplot2::theme(panel.grid.major.x = ggplot2::element_line(color = scales::alpha(RR_dark_blue, 0.5),
-                                                        linewidth = ggplot2::rel(1.75),
-                                                        linetype = "dotted"),
-                      panel.grid.major.y = ggplot2::element_line(color = scales::alpha(RR_dark_blue, 0.5),
-                                                        linewidth = ggplot2::rel(1.75),
-                                                        linetype = "dotted")))
+       ggplot2::theme(panel.grid.major.x = ggplot2::element_line(color = RR_gridlines_color,
+                                                                 linewidth = RR_gridlines_size,
+                                                                 linetype = "solid"),
+                      panel.grid.major.y = ggplot2::element_line(color = RR_gridlines_color,
+                                                                 linewidth = RR_gridlines_size,
+                                                                 linetype = "solid")))
 }
 
 #' Custom wrapper for geom_density
